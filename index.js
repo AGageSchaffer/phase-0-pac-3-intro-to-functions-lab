a7c1cbd95a45c29bbdb5b27b25fd0eafefe47da4
AustinSchaffer <austinschaffer@gmail.com>
2022-11-27 09:47:12 -0600

    commit tree d5595397f6dc49eaf2c813ad10212e65f2b678f0

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string){
+    return string
+}
+"Hello!".toUpperCase()
+function shout(string) {
+    return string.toUpperCase()
+}
+function whisper(string) {
+    return string.toLowerCase()
+}
+"HELLO".toLowerCase()
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+logShout("Hello")
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+logWhisper("Hello")
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!"
+    }
+    if (string === string.toUpperCase()) {
+        return "YES INDEED!"
+    }
+    if (string === "Let's have dinner together!") {
+        return "I would love to!"
+    }
+}
